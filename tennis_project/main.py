73c4ade90534baa99d1f559e7fb4715374fa173e
RKal-El <rmarking49@gmail.com>
2018-09-01 18:23:19 +0200

    Top 3 winners/losers per tournament

--- tennis_project/main.py
+++ tennis_project/main.py
@@ -36,8 +36,63 @@ def plot_5(winners):
     plt.show()
 
 
+def top_3_per_tournament(data, column_of_names):
+    table_player_tournament = pandas.DataFrame(0, index=set(data[column_of_names]), columns=set(data.TOURNAMENT))
+    table_player_tournament.drop(columns=['Australian Open (Jan)', 'Australian Open (Dec)'], inplace=True)
+    for index, row in data.iterrows():
+        tournament, player = row['TOURNAMENT'], row[column_of_names]
+        if (tournament != 'Australian Open (Jan)') & (tournament != 'Australian Open (Dec)'):
+            table_player_tournament[tournament].loc[player] += 1
+
+    us_open = table_player_tournament['U.S. Open'].copy()
+    australian_open = table_player_tournament['Australian Open'].copy()
+    french_open = table_player_tournament['French Open'].copy()
+    wimbledon = table_player_tournament['Wimbledon'].copy()
+
+    us_open.sort_values(inplace=True, ascending=False)
+    australian_open.sort_values(inplace=True, ascending=False)
+    french_open.sort_values(inplace=True, ascending=False)
+    wimbledon.sort_values(inplace=True, ascending=False)
+
+    return us_open[:3], australian_open[:3], french_open[:3], wimbledon[:3]
+
+
+def plot_top_3_per_tournament(add_to_the_title, *arg):
+    fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+    fig.suptitle('Top 3 ' + add_to_the_title + ' per tournament', fontsize=16)
+    plt.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.9, hspace=0.4, wspace=0.3)
+    for x_axis in range(2):
+        for y_axis in range(2):
+            if(x_axis == 0) & (y_axis == 0):
+                argument = 0
+            elif(x_axis == 0) & (y_axis == 1):
+                argument = 1
+            elif (x_axis == 1) & (y_axis == 0):
+                argument = 2
+            else:
+                argument = 3
+            x_values = list(arg[argument].index.values)
+            y_values = list(arg[argument].iloc[:])
+            axes[x_axis, y_axis].bar(x_values, y_values, width=0.25)
+            axes[x_axis, y_axis].set_title(arg[argument].name, fontsize=14)
+            for index in range(len(x_values)):
+                axes[x_axis, y_axis].text(x=index,
+                                          y=y_values[index] / 2,
+                                          s=y_values[index],
+                                          fontsize=12, horizontalalignment='center')
+    title_of_image = input('\nWhat is going to be name of image?\n')
+    title_of_image += '.png'
+    plt.savefig(title_of_image, dpi=600, format='png')
+    plt.show()
+
+
 tennis_data = import_data_from_csv_to_variable()
 top_5_victorious_players = top_5(tennis_data, 'WINNER')
 plot_5(top_5_victorious_players)
 top_5_runner_up_players = top_5(tennis_data, 'RUNNER UP')
 plot_5(top_5_runner_up_players)
+
+top_3_us_win, top_3_aus_win, top_3_fra_win, top_3_wim_win = top_3_per_tournament(tennis_data, 'WINNER')
+plot_top_3_per_tournament('winners', top_3_us_win, top_3_aus_win, top_3_fra_win, top_3_wim_win)
+top_3_us_run, top_3_aus_run, top_3_fra_run, top_3_wim_run = top_3_per_tournament(tennis_data, 'RUNNER UP')
+plot_top_3_per_tournament('runner up', top_3_us_run, top_3_aus_run, top_3_fra_run, top_3_wim_run)
